1288de3443c8368e0cd3aa31981af231d14659a1
Diego Menchaca <dmenchaca@users.noreply.github.com>
2024-12-29 16:30:31 +0100

    Removed html implementation of widget

--- src/lib/feedback-widget/logger.ts
+++
@@ -1,9 +0,0 @@
-export const Logger = {
-  debug: (message: string, ...args: any[]) => {
-    console.log(`[Userbird Debug] ${message}`, ...args);
-  },
-  
-  error: (message: string, ...args: any[]) => {
-    console.error(`[Userbird Error] ${message}`, ...args);
-  }
-};
