0b8a00c30b1653a15403bad32409f66162a5b5f5
Diego Menchaca <dmenchaca@users.noreply.github.com>
2025-01-08 10:55:28 -0300

    Moved cache implementation to functions directory

--- netlify/functions/form-settings/index.ts
+++ netlify/functions/form-settings/index.ts
@@ -1,6 +1,6 @@
 import { Handler } from '@netlify/functions';
 import { createClient } from '@supabase/supabase-js';
-import { cache } from '../../src/lib/cache';
+import { cache } from '../lib/cache';
 
 const supabaseUrl = process.env.VITE_SUPABASE_URL!;
 const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY!;
