fc0dd0af1296be530d8c940230342ea4a45c8b29
Diego Menchaca <dmenchaca@users.noreply.github.com>
2025-03-17 15:02:48 +0100

    Integrated PostHog

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,23 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { PostHogProvider } from 'posthog-js/react'
 import App from './App.tsx'
 import './index.css'
 
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PostHogProvider 
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={options}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PostHogProvider>
   </StrictMode>
 )
