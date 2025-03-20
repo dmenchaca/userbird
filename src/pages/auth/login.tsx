365a0e9050e26fb25126dac21520eef7934144cb
Diego Menchaca <dmenchaca@users.noreply.github.com>
2025-03-20 20:26:56 +0100

    Fixed login page vertical scroll

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -18,9 +18,9 @@ export function LoginPage() {
   }, []);
 
   return (
-    <div className="container relative h-screen grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+    <div className="container relative h-screen grid lg:max-w-none lg:grid-cols-2 lg:px-0 overflow-hidden">
       <div className="lg:p-8">
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px] h-full">
           <div className="flex justify-center pb-4">
             <a href="/" className="flex items-center gap-2 font-medium">
               <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
