8900119b322fb6b46b1afa408f45637350c1f93b
Diego Menchaca <dmenchaca@users.noreply.github.com>
2025-03-24 12:05:26 +0100

    Added gif on succes to form settings json

--- netlify/functions/form-settings/index.ts
+++ netlify/functions/form-settings/index.ts
@@ -35,7 +35,7 @@ export const handler: Handler = async (event) => {
 
     const { data, error } = await supabase
       .from('forms')
-      .select('url, button_color, support_text, keyboard_shortcut, sound_enabled')
+      .select('url, button_color, support_text, keyboard_shortcut, sound_enabled, gif_on_success')
       .eq('id', formId.trim())
       .single();
 
