b12278c7af0bd2aefde10286ae12c7bc016834df
Diego Menchaca <dmenchaca@users.noreply.github.com>
2025-03-26 21:15:01 +0100

    Updated text for clarity and added technology icons to the auth page for improved visual appeal.

--- src/pages/auth/auth.tsx
+++ src/pages/auth/auth.tsx
@@ -67,8 +67,14 @@ export function AuthPage() {
               <div className="flex flex-col items-start max-w-[210px]">
                 <CodeXml className="h-6 w-6 mb-2 text-primary" />
                 <p className="text-black">
-                  Install widget. <span className="text-black opacity-40">Takes less than 5 minutes.</span>
+                  Install widget. <span className="text-black opacity-40">It takes less than 5 minutes.</span>
                 </p>
+                <div className="flex gap-3 pt-4">
+                  <img src="/react.svg" alt="React" className="w-[20px] h-[20px]" />
+                  <img src="/vue.svg" alt="Vue" className="w-[20px] h-[20px]" />
+                  <img src="/angular.svg" alt="Angular" className="w-[20px] h-[20px]" />
+                  <img src="/js.svg" alt="JavaScript" className="w-[20px] h-[20px]" />
+                </div>
               </div>
               <div className="flex flex-col items-start max-w-[210px]">
                 <Rocket className="h-6 w-6 mb-2 text-primary" />
