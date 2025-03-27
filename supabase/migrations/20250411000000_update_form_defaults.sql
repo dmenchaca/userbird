a4597f873031bc83225f80951e6f2c10fd09f027
Diego Menchaca <dmenchaca@users.noreply.github.com>
2025-03-27 09:52:36 +0100

    Update form defaults to enable sound and set default GIF URLs for new forms

--- supabase/migrations/20250411000000_update_form_defaults.sql
+++ supabase/migrations/20250411000000_update_form_defaults.sql
@@ -4,6 +4,8 @@
   1. Changes
     - Change show_gif_on_success default to true instead of false
     - Set keyboard_shortcut default to 'L'
+    - Set default GIF URLs for all new forms
+    - Enable sound by default
   
   2. Notes
     - Updates defaults for all new forms
@@ -18,5 +20,16 @@ ALTER COLUMN show_gif_on_success SET DEFAULT true;
 ALTER TABLE forms 
 ALTER COLUMN keyboard_shortcut SET DEFAULT 'L';
 
--- We don't set a default for gif_urls as it's an array
--- The application code will handle setting the default values 
+-- Enable sound by default
+ALTER TABLE forms
+ALTER COLUMN sound_enabled SET DEFAULT true;
+
+-- Set default GIFs for gif_urls column
+ALTER TABLE forms 
+ALTER COLUMN gif_urls SET DEFAULT ARRAY[
+  'https://media1.tenor.com/m/TqHquUQoqu8AAAAd/you%27re-a-lifesaver-dove.gif',
+  'https://media1.tenor.com/m/4PLfYPBvjhQAAAAd/tannerparty-tanner.gif',
+  'https://media1.tenor.com/m/lRY5I7kwR08AAAAd/brooklyn-nine-nine-amy-and-rosa.gif',
+  'https://media1.tenor.com/m/9LbEpuHBPScAAAAd/brooklyn-nine-nine-amy-and-rosa.gif',
+  'https://media1.tenor.com/m/mnx8ECSie6EAAAAd/sheldon-cooper-big-bang-theory.gif'
+]::text[]; 
