d1f6e189dbe37ad1d4cf02185125d01091e43ca3
Diego Menchaca <dmenchaca@users.noreply.github.com>
2025-04-10 16:46:49 +0200

    Remove unused import in FixInvitation component

--- src/pages/FixInvitation.tsx
+++ src/pages/FixInvitation.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { acceptInvitationById } from '@/lib/utils/invitations';
 
